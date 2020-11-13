aabe5d1cc0c92b597860da4b6be764c310276087
Roseak-lin <roseak.lin@gmail.com>
2020-11-13 16:37:46 -0500

    Fixed timer bug
    Updated Repo

--- activity.js
+++ activity.js
@@ -1,4 +1,4 @@
-var count = 0, best = Infinity, sumOfTimes = 0, attempts = 0;
+var count = 0, best = Infinity, sumOfTimes = 0, attempts = 0, blueTilesClicked = 0;
 var start, end;
 
 function buildTable() {
@@ -28,19 +28,20 @@ function buildTable() {
 
 function tileClick(x, y) {
     document.getElementById(y).disabled = true;
-    document.getElementById(y).setAttribute("style", "background-color: white; height: 150px; width: 150px");
+    document.getElementById(y).setAttribute("style", "background-color: white; height: 15vh; width: 15vh");
     let html = "";
-  
+
     if (x == 1) {
         count++;
         if (count == 4) {
             end = performance.now();
             attempts++;
-            sumOfTimes += ((end - start)) - 0.0005;
-            best = Math.min(best, ((end - start)) - 0.0005);
-            html += "Your last attempt was " + ((end - start)).toFixed(0) + " ms<br><br>";
+            let time = end - start + (50 * blueTilesClicked);
+            sumOfTimes += time - 0.0005;
+            best = Math.min(best, time - 0.0005);
+            html += "Your last attempt was " + ((time).toFixed(0)) + " ms; blue tiles clicked last attempt: " + blueTilesClicked + "<br><br>";
             html += "<b>Your best time so far is: " + best.toFixed(0) + " ms</b><br>";
-            html += "<b>Your average time is: " + (sumOfTimes/attempts).toFixed(0) + " ms</b>";
+            html += "<b>Your average time is: " + (sumOfTimes / attempts).toFixed(0) + " ms</b>";
             document.getElementById("HighScore").innerHTML = html;
 
             // disable buttons 
@@ -48,20 +49,20 @@ function tileClick(x, y) {
             for (let i = 0; i < blue.length; i++) {
                 blue[i].disabled = true;
             }
-            // reset counter
-            count = 0;
         }
+    } else {
+        blueTilesClicked++;
     }
 }
 
 
 function countdown() {
-    // disable to prevent multiple countdowns from starting
+    count = 0;
+    blueTilesClicked = 0;
     document.getElementsByClassName("start")[0].disabled = true;
-    // placeholder for grid
     let html = "<table>";
     for (let i = 0; i < 4; i++) {
-        html += "<tr style='height: 150px'/>";
+        html += "<tr style='height: 15vh'/>";
     }
     html += "</table>";
     document.getElementById("grid").innerHTML = html;
